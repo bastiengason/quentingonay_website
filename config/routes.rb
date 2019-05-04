450c179ca6f35b7cc68664d8231c2aa1ba546c4b
Bastien Gason <gasonbastien@gmail.com>
2019-05-04 20:07:42 +0200

    setting up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'application#home'
+
+  # get '/creator', to: 'pages#creator'
+  # get '/contact', to: 'pages#contact'
+  # get '/hire', to: 'pages#hire'
+  # get '/about', to: 'pages#about'
 end
